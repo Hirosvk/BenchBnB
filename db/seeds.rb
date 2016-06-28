2e0c113ee5dbcb185b91e5786dc8ad1d98a1cc18
App Academy Student <student@appacademy.io>
2016-06-27 17:55:49 -0700

    working on error store

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Bench.create!(description: "Check out this cool yellow bench.", lat: 37.754226, lng: -122.440193)
-# Bench.create!(description: "Check out this cool green bench.", lat: 37.755727, lng: -122.442929)
-# Bench.create!(description: "Check out this cool rainbow bench.", lat: 37.767186, lng: -122.435747)
-# Bench.create!(description: "Check out this cool giant bench.", lat: 37.773177, lng: -122.441176)
-# Bench.create!(description: "Check out this cool black bench.", lat: 37.780763, lng: -122.441042)
-# Bench.create!(description: "Check out this cool metal bench.", lat: 37.782320, lng: -122.435162)
-# Bench.create!(description: "Check out this cool brick bench.", lat: 37.790952, lng: -122.427055)
+Bench.create!(description: "Check out this cool yellow bench.", lat: 37.754226, lng: -122.440193)
+Bench.create!(description: "Check out this cool green bench.", lat: 37.755727, lng: -122.442929)
+Bench.create!(description: "Check out this cool rainbow bench.", lat: 37.767186, lng: -122.435747)
+Bench.create!(description: "Check out this cool giant bench.", lat: 37.773177, lng: -122.441176)
+Bench.create!(description: "Check out this cool black bench.", lat: 37.780763, lng: -122.441042)
+Bench.create!(description: "Check out this cool metal bench.", lat: 37.782320, lng: -122.435162)
+Bench.create!(description: "Check out this cool brick bench.", lat: 37.790952, lng: -122.427055)
+
+User.create!(username: "Hiro", password: "hirohiro");
+User.create!(username: "Nasley", password: "nasleynasley");
+User.create!(username: "Seiji", password: "koikoi");
